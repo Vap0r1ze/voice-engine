1333e0fca39252624284fa2c50f19232546a5098
Vendicated <vendicated@riseup.net>
2023-01-16 00:28:41 +0100

    Expose segfault function to node

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,14 @@ fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
 
+fn segfault(mut _cx: FunctionContext) -> JsResult<JsString> {
+  segfault::segfault();
+  unreachable!()
+}
+
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("hello", hello)?;
+    cx.export_function("segfault", segfault)?;
     Ok(())
 }
